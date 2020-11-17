0115cb82deaf2d478f614d791dc34d8f22dc15fb
Mijeong Ban <mban1@stevens.edu>
2020-11-17 16:27:54 -0500

    review useEffect

--- tours/src/App.js
+++ tours/src/App.js
@@ -1,11 +1,36 @@
 import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
+
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true)
+  const [tours, setTours] = useState([])
+
+  const fetchTours = async () => {
+    setLoading(true)
+    const response = await fetch(url)
+    const tours = await response.json();
+    console.log(tours)
+  }
+
+  /**
+   * By default, it runs after every re-render
+   * Callback function 
+   * Second parameter: []; run only on initial render
+   *                    [something]; re-run the errect if something changes 
+   * You can have multiple useEffect functions 
+   */
+  useEffect(() => {
+    fetchTours();
+  }, [])
+
+  if (loading) {
+    return <Loading />
+  } 
+
+  return (<main><Tours /></main>)
 }
 
 export default App
